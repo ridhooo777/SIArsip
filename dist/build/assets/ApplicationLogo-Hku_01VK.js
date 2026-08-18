7d44626f010f802826ed79f5573f260a90f48e8a
ridhooo777 <ridodudin07@gmail.com>
2026-08-18 15:08:37 +0700

    Fix Vercel build output directory and node types peer dependency conflict

---
+++ dist/build/assets/ApplicationLogo-Hku_01VK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-vSTlYAA1.js";function n(i){return o.jsx("img",{src:"/images/logo-piksi.png",alt:"Politeknik Piksi Input Serang",...i})}export{n as A};
--- dist/build/assets/ApplicationLogo-koDTJIgP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-CpZcUin4.js";function n(i){return o.jsx("img",{src:"/images/logo-piksi.png",alt:"Politeknik Piksi Input Serang",...i})}export{n as A};
